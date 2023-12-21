eb53c47042bdd72855250e4fa4631c60f7004696
nishaz <2459nishasenthil@gmail.com>
2023-12-21 22:04:30 +0800

    firebase setup

--- skippyQ/src/app/app.component.ts
+++ skippyQ/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  platform: any;
+  constructor() {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyAuHI89NT0-8o2zD0IoeXD1wiwuTxcHeKQ",
+      authDomain: "karang-guni-d3f15.firebaseapp.com",
+      projectId: "karang-guni-d3f15",
+      storageBucket: "karang-guni-d3f15.appspot.com",
+      messagingSenderId: "905280167915",
+      appId: "1:905280167915:web:dc0aecdd837912f4ca03ae"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  initializeApp(){
+
+    this.platform.ready().then(() => {
+
+  });
+  }
 }
