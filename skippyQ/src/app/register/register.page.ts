67950dadea1664924168dec7f9018f77a8b14367
nishaz <2459nishasenthil@gmail.com>
2024-01-01 21:52:28 +0800

    create validator service for form validation. Add form validation for registration page, including password strength checker and password match.

--- skippyQ/src/app/register/register.page.ts
+++ skippyQ/src/app/register/register.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+import { Profile } from '../shared/services/models/profile';
+import { ToastController } from '@ionic/angular';
+import { ValidatorsService } from '../shared/services/validators.service';
+import { startWith } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -7,16 +13,104 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-registerForm: FormGroup;
+  registerForm: FormGroup;
+  submitted: boolean = false;
+  ageranges: string[];
 
-  constructor() {
-    this.registerForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl('')
-    }); }
-  
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private validService: ValidatorsService,
+    private toastController: ToastController
+  ) {
+    this.registerForm = new FormGroup(
+      {
+        name: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        phone: new FormControl('', [Validators.required, this.validService.validPhoneNo]),
+        password: new FormControl('', [Validators.required]),
+        cfmpassword: new FormControl('', [Validators.required]),
+        ageRange: new FormControl('', [Validators.required])
+      },
+      { validators: this.validService.matchPasswords }
+    );
 
-  ngOnInit() {
+    this.ageranges = ['20-29', '30-39', '40-49', '50-59', '60 And Above'];
   }
 
+  ngOnInit() {
+    // Listen for password changes and perform validations
+    this.registerForm.controls['password'].valueChanges.pipe(startWith('')).subscribe((value) => {
+      this.validatePasswordStrength(value);
+      this.validatePasswordMatch();
+    });
+
+    this.registerForm.controls['cfmpassword'].valueChanges.pipe(startWith('')).subscribe(() => {
+      this.validatePasswordMatch();
+    });
+  }
+
+  async presentToast(msg: string, color: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 3000, // Display for 3 seconds
+      position: 'top',
+      color: color,
+      cssClass: 'custom-toast', // Add a custom class for further styling if needed
+    });
+
+    toast.present();
+  }
+
+  register() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      const newProfile = new Profile(
+        '',
+        false,
+        this.registerForm.value.email,
+        this.registerForm.value.phone,
+        this.registerForm.value.name,
+        this.registerForm.value.password,
+        this.registerForm.value.ageRange
+      );
+
+      this.authService
+        .register(newProfile)
+        .then((userCredential) => {
+          // Extract UID from the userCredential
+          const uid = userCredential.user?.uid;
+
+          // Do anything else you need with the UID
+          console.log('User ID:', uid);
+
+          this.router.navigate(['login']);
+        })
+        .catch((error) => {
+          // Handle registration errors
+          console.error('Registration error:', error);
+          this.presentToast(error.message, 'danger');
+        });
+    }
+  }
+
+  validatePasswordStrength(password: string) {
+    const isPasswordStrong = this.validService.isPasswordStrong(password);
+    if (!isPasswordStrong) {
+      this.registerForm.controls['password'].setErrors({ weakPassword: true });
+    } else {
+      this.registerForm.controls['password'].setErrors(null);
+    }
+  }
+
+  validatePasswordMatch() {
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('cfmpassword')?.value;
+
+    if (password !== confirmPassword) {
+      this.registerForm.controls['cfmpassword'].setErrors({ passwordMismatch: true });
+    } else {
+      this.registerForm.controls['cfmpassword'].setErrors(null);
+    }
+  }
 }
---
+++ skippyQ/src/app/shared/services/validators.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidatorsService {
+
+  constructor() { }
+
+  matchPasswords(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get('password')?.value; //form input name must be the same
+    const confirmPassword = control.get('cfmpassword')?.value;
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+  
+    return password === confirmPassword ? null : { 'passwordMismatch': true };
+  }
+
+  isPasswordStrong(password: string) {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return passwordRegex.test(password);
+  }
+
+  validPhoneNo(control: AbstractControl): { [key: string]: boolean } | null {
+    const phonePattern = /^\d{8}$/; // Assuming an 8-digit phone number format
+
+    const isInvalid = !phonePattern.test(control.value);
+
+    return isInvalid ? { 'invalidPhone': true } : null;
+  }
+}
+
+
