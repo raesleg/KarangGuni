3c94362f4cd8da05d3f653d7dbf51c4f74165e1d
nishaz <2459nishasenthil@gmail.com>
2024-01-05 00:02:31 +0800

    retrive current user's name in home and profile page to display name in html

--- skippyQ/src/app/tab3/tab3.page.ts
+++ skippyQ/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+userName: string | undefined;
 
-  constructor() {}
-
+  constructor(private authService: AuthService) {
+    const currentUser = this.authService.getCurrentUser();
+    if (currentUser && currentUser.email) {
+      try {
+        this.authService.getUserProfile(currentUser.email).then((profile) => {
+          // Use the retrieved profile to get the name
+          if (profile) {
+            this.userName = profile.name;
+            console.log('User Name:', this.userName);
+          } else {
+            console.log('User profile not found');
+          }
+        });
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    } else {
+      console.log('Could not get current user');
+    }
+  }
 }
+  
+
