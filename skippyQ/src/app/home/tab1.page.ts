9327eabd097bffff8ac040c508128c159c49fb26
anna <zanna.oop@gmail.com>
2024-02-13 04:27:09 +0800

    home page concurrent user fix

--- skippyQ/src/app/home/tab1.page.ts
+++ skippyQ/src/app/home/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../shared/services/user.service';
 import firebase from 'firebase/app';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-tab1',
@@ -15,9 +16,29 @@ export class Tab1Page {
   specificGroupIds: string[] = ['HHlg9xJbdOHTTP02U3OT', 'iY8n9ARTcJ5avyBitdGJ'];
   vouchers: any[] = [];
 
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService) { }
 
   async ngOnInit() {
+
+    this.authService.observeAuthState(user => {
+      if (user && user.email) {
+        // If a user is logged in, get the user profile
+        this.authService.getUserProfile(user.email).then((profile) => {
+          if (profile) {
+            this.userName = profile.name
+          } else {
+            console.log('User profile not found');
+          }
+        }).catch(error => {
+          console.error('Error fetching user profile:', error);
+        });
+      } else {
+        console.log('No user logged in');
+      }
+    });
+
     const userData = await this.userService.getUserData();
     if (userData) {
       this.userName = userData.name; // Assuming 'name' is the field in the Firestore document
